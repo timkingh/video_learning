5386e41354f185eb06afaae02f0642ce34543722
timkingh <timkingh@163.com>
2016-03-17 21:34:59 +0800

    001 在一个类（A）中嵌入另一个类（B）。 （1）数据成员按照它们在类定义中的出现顺序进行初始化：m_Matter比m_identifier 更早进行初始化 （2）数据成员的初始化顺序与前导顺序无关

--- main.cpp
+++ main.cpp
@@ -2,25 +2,55 @@
 
 using namespace std;
 
+
+class Matter
+{
+public:
+	Matter(int idx):m_id(idx)
+	{
+		cout << m_id << ":" << "This is a matter!\n";
+	}
+
+	~Matter()
+	{
+		cout << m_id << ":" << "matter destructor!\n";
+	}
+
+private:
+	const int m_id;
+};
+
+
 class World
 {
 public:
-	World()
+	World(int idx):m_Matter(idx),m_identifier(idx)
 	{
-		cout << "Hello!" << endl;
+		cout << m_identifier << ":" << "Hello!" << endl;
 	}
 
 	~World()
 	{
-		cout << "Good Bye!" << endl;
+		cout << m_identifier << ":" << "Good Bye!" << endl;
 	}
+
+	Matter m_Matter;
+
+private:
+	const int m_identifier;	
 };
 
-World TheWorld;
+World TheWorld(1);
 
 int main()
-{
+{	
+	World smallWorld(2);
 	cout << "This is Main Function!" << endl;
+
+	for(int i = 3; i < 5;i++)
+	{
+		//World aWorld(i);
+	}
     return 0;
 }
 
