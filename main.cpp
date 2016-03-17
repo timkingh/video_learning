eb803d5a4d57502685d18edb11556af059cf9f0d
timkingh <timkingh@163.com>
2016-03-17 23:07:21 +0800

    002 （1）基类和派生类  （2）成员函数和接口

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 
+/*001 在自定义数据类型中嵌入其他自定义数据类型:比较数据成员的初始化顺序
+**    (1) 析构函数不能带参数
+**    (2) 数据成员按照类定义中的顺序进行初始化
+*/
+#if 0
 class Matter
 {
 public:
@@ -37,20 +42,97 @@ public:
 	Matter m_Matter;
 
 private:
-	const int m_identifier;	
+	const int m_identifier;
 };
 
 World TheWorld(1);
+#endif
+
+
+/*002 类的继承: 比较基类和派生类的构造函数及析构函数的调用顺序
+**    (1)基类的构造函数先于派生类被调用
+**    (2)派生类的析构函数先于基类被调用
+*/
+#if 0
+class CelestialBody
+{
+public:
+	CelestialBody(float mass):m_mass(mass)
+	{
+		cout << "CelestialBody Constructor from " << m_mass << endl;
+	}
+
+	~CelestialBody()
+	{
+		cout << "CelestialBody Destructor from " << m_mass << endl;
+	}
+
+private:
+	const float m_mass;
+};
+
+
+class Star:public CelestialBody
+{
+public:
+	Star(float mass,float brightness):CelestialBody(mass),m_brightness(brightness)
+	{
+		cout << "Star Constructor from " << m_brightness << endl;
+	}
+
+	~Star()
+	{
+		cout << "Star Destructor from " << m_brightness << endl;
+	}
+
+private:
+	const float m_brightness;
+};
+#endif
+
+
+/*003 成员函数和接口,成员函数的作用域
+**    (1)使用成员函数访问数据成员没有运行时开销,所以没有太多理由在程序中使用公有数据成员
+*/
+#if 0
+class InputNum
+{
+public:
+	InputNum(char aString[])
+	{
+		cout << aString << endl;
+		cin >> m_num;
+	}
+
+	int GetValue()
+	{
+		return m_num;
+	}
+
+	void AddNum(char bString[])
+	{
+		InputNum aInputNum(bString);
+		m_num += aInputNum.GetValue();
+	}
+
+	~InputNum()
+	{
+		cout << "destructor from " << m_num << endl;
+	}
+
+private:
+	int m_num;
+};
+#endif
+
+
+/*004 
+**
+*/
+
 
 int main()
-{	
-	World smallWorld(2);
-	cout << "This is Main Function!" << endl;
-
-	for(int i = 3; i < 5;i++)
-	{
-		//World aWorld(i);
-	}
+{
     return 0;
 }
 
