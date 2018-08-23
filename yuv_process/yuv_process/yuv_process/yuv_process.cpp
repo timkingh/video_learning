01e2624e6167094dc37df5583b0740a596705ac5
timkingh.huang <timkingh.huang@rock-chips.com>
2018-08-23 15:53:06 +0800

    Draw red rectangle
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- yuv_process/yuv_process/yuv_process/yuv_process.cpp
+++ yuv_process/yuv_process/yuv_process/yuv_process.cpp
@@ -7,7 +7,8 @@ using namespace std;
 void main(int argc, char **argv)
 {
 	cout << "----------Test-------------" << endl;
-	unsigned int luma_size = 352 * 288;
+	unsigned int width = 352, height = 288;
+	unsigned int luma_size = width * height;
 	unsigned int chroma_size = luma_size / 2;
 	unsigned int frame_size = luma_size + chroma_size;
     char *buf = new char[frame_size];
@@ -17,16 +18,49 @@ void main(int argc, char **argv)
 
 	istream *ifs = new ifstream(in_path.c_str(), ios::binary | ios::in);
 	ifs->read(buf, frame_size);
-	for (idx = 0; idx < luma_size; idx++) {
-		buf[idx] = 76;
+
+	unsigned int x0, y0, x1, y1;
+	char *buf_y = buf;
+	char *buf_u = buf + luma_size;
+	char *buf_v = buf_u + chroma_size / 2;
+	x0 = y0 = 10;
+	x1 = y1 = 100;
+
+	char *buf_top = buf_y + x0 + y0 * width;
+	char *buf_bottom = buf_y + x0 + y1 * width;
+	for (idx = 0; idx < x1 - x0 + 1; idx++) {
+		buf_top[idx] = buf_bottom[idx] = 76;
 	}
 
-	for (idx = luma_size; idx < luma_size + chroma_size / 2; idx++) {
-		buf[idx] = 84;
+	char *buf_left = buf_top;
+	char *buf_right = buf_left + (x1 - x0);
+	for (idx = 0; idx < y1 - y0 + 1; idx++) {
+		buf_left[idx * width] = buf_right[idx * width] = 76;
 	}
-	
-	for (idx = luma_size + chroma_size / 2; idx < frame_size; idx++) {
-		buf[idx] = 255;
+
+	unsigned int chroma_width = width / 2;
+	buf_top = buf_u + x0 + y0 * chroma_width;
+	buf_bottom = buf_u + x0 + y1 * chroma_width;
+	for (idx = 0; idx < x1 - x0 + 1; idx++) {
+		buf_top[idx] = buf_bottom[idx] = 84;
+	}
+
+	buf_left = buf_top;
+	buf_right = buf_left + (x1 - x0);
+	for (idx = 0; idx < y1 - y0 + 1; idx++) {
+		buf_left[idx * chroma_width] = buf_right[idx * chroma_width] = 84;
+	}
+
+	buf_top = buf_v + x0 + y0 * chroma_width;
+	buf_bottom = buf_v + x0 + y1 * chroma_width;
+	for (idx = 0; idx < x1 - x0 + 1; idx++) {
+		buf_top[idx] = buf_bottom[idx] = 255;
+	}
+
+	buf_left = buf_top;
+	buf_right = buf_left + (x1 - x0);
+	for (idx = 0; idx < y1 - y0 + 1; idx++) {
+		buf_left[idx * chroma_width] = buf_right[idx * chroma_width] = 255;
 	}
 
 	string out_path = "F:\\rkvenc_verify\\input_yuv\\Red_352x288_25.yuv";
