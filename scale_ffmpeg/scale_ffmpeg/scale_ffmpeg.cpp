02893e5865e7c7ce22cda5e65dfbfc6ff5da9dad
timkingh <timkingh@163.com>
2018-08-26 22:54:28 +0800

    swscale works
    Signed-off-by: timkingh <timkingh@163.com>

--- scale_ffmpeg/scale_ffmpeg/scale_ffmpeg.cpp
+++ scale_ffmpeg/scale_ffmpeg/scale_ffmpeg.cpp
@@ -1,7 +1,16 @@
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "stdint.h"
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
+}
 
 using namespace std;
 
@@ -20,11 +29,46 @@ int main()
     uint32_t frames = 10, frame_cnt = 0;
     uint32_t width = 1280, height = 720;
     uint32_t frame_size = width * height * 3 / 2;
+    uint32_t y_size = width * height;
     char *buf = new char[frame_size];
+    uint8_t *in_data[3];
+    uint32_t out_width = 1280, out_height = 720;
+    in_data[0] = (uint8_t *)buf;
+    in_data[1] = in_data[0] + y_size;
+    in_data[2] = in_data[1] + y_size / 4;
+
+    cout << "log level " << av_log_get_level() << endl;
+    av_log_set_level(56);
+    cout << "log level " << av_log_get_level() << endl;
+
+    AVFrame *pFrameYUV;
+    unsigned char *out_buffer;
+    int linesize[3] = {0};
+    linesize[0] = width;
+    linesize[1] = linesize[2] = width / 2;
+    pFrameYUV = av_frame_alloc();
+
+    uint32_t out_frame_size = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, out_width, out_height, 8);
+    cout << "buffer size " << out_frame_size << endl;
+    out_buffer = (unsigned char *)av_malloc(out_frame_size);
+    av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, out_buffer,
+                         AV_PIX_FMT_YUV420P, out_width, out_height, 8);
+
+    struct SwsContext *img_convert_ctx;
+    img_convert_ctx = sws_getContext(width, height, AV_PIX_FMT_YUV420P,
+                                     out_width, out_height, AV_PIX_FMT_YUV420P,
+                                     SWS_BICUBIC, NULL, NULL, NULL);
 
     while (frame_cnt++ < frames) {
         ifs->read(buf, frame_size);
-        ofs.write(buf, frame_size);
+
+        sws_scale(img_convert_ctx, (const unsigned char* const*)in_data,
+                  linesize, 0, height,
+                  pFrameYUV->data, pFrameYUV->linesize);
+        cout << "frame_cnt " << frame_cnt << endl;
+        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[0]), y_size);
+        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[1]), y_size / 4);
+        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[2]), y_size / 4);
     }
 
     cout << "End!" << endl;
