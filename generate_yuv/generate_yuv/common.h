e39368ea03415ad501461f20345d3d82881b9cc4
timkingh.huang <timkingh.huang@rock-chips.com>
2018-09-19 16:45:11 +0800

    fill first CU4x4
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- generate_yuv/generate_yuv/common.h
+++ generate_yuv/generate_yuv/common.h
@@ -3,12 +3,14 @@
 
 #include <stdint.h>
 
+typedef struct BufInfo {
+    char *buf;
+    uint32_t width;
+    uint32_t height;
+} BufInfo;
+
+
 int64_t time_usec(void);
-
-
-
-
-
 
 
 
--- generate_yuv/generate_yuv/generate_yuv.cpp
+++ generate_yuv/generate_yuv/generate_yuv.cpp
@@ -15,6 +15,80 @@ using namespace std;
 #define SSCANF sscanf
 #endif
 
+typedef struct BlkInfo {
+    uint32_t y_left;
+    uint32_t y_top;
+    uint32_t uv_left;
+    uint32_t uv_top;
+    uint32_t mb_size;
+    uint32_t y_pixel;
+    uint32_t u_pixel;
+    uint32_t v_pixel;
+} BlkInfo;
+
+static void fill_block(BufInfo *buf_info, BlkInfo *blk_info)
+{
+    uint32_t col, row;
+    uint32_t x0, y0, mb_size;
+    uint32_t width = buf_info->width;
+    uint32_t height = buf_info->height;
+    char *buf_y = buf_info->buf;
+    char *buf_u = buf_info->buf + width * height;
+    char *buf_v = buf_u + width * height / 4;
+    x0 = blk_info->y_left;
+    y0 = blk_info->y_top;
+    mb_size = blk_info->mb_size;
+
+    /* Y */
+    char *buf = buf_y + x0 + y0 * width;
+    for (row = 0; row < mb_size; row++) {
+        for (col = 0; col < mb_size; col++) {
+            buf[row * width + col] = blk_info->y_pixel;
+        }
+    }
+
+    uint32_t chroma_width = width / 2;
+    x0 = blk_info->uv_left;
+    y0 = blk_info->uv_top;
+
+    /* U */
+    buf = buf_u + x0 + y0 * chroma_width;
+    for (row = 0; row < mb_size; row++) {
+        for (col = 0; col < mb_size; col++) {
+            buf[row * chroma_width + col] = blk_info->u_pixel;
+        }
+    }
+
+    /* V */
+    buf = buf_v + x0 + y0 * chroma_width;
+    for (row = 0; row < mb_size; row++) {
+        for (col = 0; col < mb_size; col++) {
+            buf[row * chroma_width + col] = blk_info->v_pixel;
+        }
+    }
+}
+
+
+static void fill_blocks(GenCtx *ctx, char *buf)
+{
+    BlkInfo blk_info;
+    blk_info.y_left = 0;
+    blk_info.y_top = 0;
+    blk_info.uv_left = 0;
+    blk_info.uv_top = 0;
+    blk_info.mb_size = 4;
+    blk_info.y_pixel = 0;
+    blk_info.u_pixel = 0;
+    blk_info.v_pixel = 0;
+
+    BufInfo buf_info;
+    buf_info.buf = buf;
+    buf_info.width = ctx->width;
+    buf_info.height = ctx->height;
+
+    fill_block(&buf_info, &blk_info);
+}
+
 static void generate_yuv(GenCtx *ctx)
 {
     uint32_t luma_size = ctx->width * ctx->height;
@@ -25,6 +99,8 @@ static void generate_yuv(GenCtx *ctx)
 
     do {
         ctx->ifs->read(buf, frame_size);
+
+        fill_blocks(ctx, buf);
 
         ctx->ofs->write(buf, frame_size);
 
