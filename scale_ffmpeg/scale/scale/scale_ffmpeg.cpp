0dbb59767982f8bf30efac087d38802a67886499
timkingh.huang <timkingh.huang@rock-chips.com>
2018-12-14 10:28:38 +0800

    convert yuv420sp to rgb888
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- scale_ffmpeg/scale/scale/scale_ffmpeg.cpp
+++ scale_ffmpeg/scale/scale/scale_ffmpeg.cpp
@@ -24,9 +24,30 @@ typedef struct CliCtx {
     uint32_t height_out;
     uint8_t *in_data[3];
 
+    ofstream *ofs;
+
+    AVFrame *dst_frame;
+
     enum AVPixelFormat src_fmt;
     enum AVPixelFormat dst_fmt;
 } CliCtx;
+
+static void write_dst_frame_to_file(CliCtx *ctx)
+{
+    AVFrame *dst = ctx->dst_frame;
+    uint32_t width = ctx->width_in;
+    uint32_t height = ctx->height_in;
+
+    if (ctx->dst_fmt == AV_PIX_FMT_YUV420P) {
+        uint32_t y_size = ctx->width_in * ctx->height_in;
+        ctx->ofs->write(reinterpret_cast<char*>(dst->data[0]), y_size);
+        ctx->ofs->write(reinterpret_cast<char*>(dst->data[1]), y_size / 4);
+        ctx->ofs->write(reinterpret_cast<char*>(dst->data[2]), y_size / 4);
+    } else if (ctx->dst_fmt == AV_PIX_FMT_RGB24) {
+        ctx->ofs->write(reinterpret_cast<char*>(dst->data[0]),
+                        dst->linesize[0] * height);
+    }
+}
 
 
 int main(int argc, char **argv)
@@ -44,13 +65,14 @@ int main(int argc, char **argv)
     ctx->height_in = getarg(288, "-h", "--height");
     ctx->frames = getarg(10, "-f", "--frames");
     ctx->src_fmt = static_cast<enum AVPixelFormat>(getarg(25, "-src_fmt"));
-	ctx->dst_fmt = static_cast<enum AVPixelFormat>(getarg(0, "-dst_fmt"));
+	ctx->dst_fmt = static_cast<enum AVPixelFormat>(getarg(2, "-dst_fmt"));
 
     cout << "input: " << in_path << endl
          << "output: " << out_path << endl
          << "frames: " << ctx->frames << endl;
     istream *ifs = new ifstream(in_path.c_str(), ios::binary | ios::in);
     ofstream ofs;
+    ctx->ofs = &ofs;
     ofs.open(out_path.c_str(), ios::binary | ios::out);
 
     uint32_t frame_cnt = 0;
@@ -64,11 +86,10 @@ int main(int argc, char **argv)
     in_data[1] = in_data[0] + ctx->width_in * ctx->height_in;
     in_data[2] = in_data[0] + ctx->width_in * ctx->height_in;
 
-    AVFrame *pFrameYUV;
     unsigned char *out_buffer;
     int src_linesize[4] = {0};
 
-    pFrameYUV = av_frame_alloc();
+    ctx->dst_frame = av_frame_alloc();
     ret = av_image_fill_linesizes(src_linesize, ctx->src_fmt, ctx->width_in);
     if (ret < 0) {
         cout << "fill src linesize error ret " << ret << endl;
@@ -77,7 +98,7 @@ int main(int argc, char **argv)
     uint32_t out_frame_size = av_image_get_buffer_size(ctx->dst_fmt, out_width, out_height, 1);
     cout << "buffer size " << out_frame_size << endl;
     out_buffer = (unsigned char *)av_malloc(out_frame_size);
-    av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize, out_buffer,
+    av_image_fill_arrays(ctx->dst_frame->data, ctx->dst_frame->linesize, out_buffer,
                          ctx->dst_fmt, out_width, out_height, 1);
 
     struct SwsContext *img_convert_ctx;
@@ -85,17 +106,14 @@ int main(int argc, char **argv)
                                      out_width, out_height, ctx->dst_fmt,
                                      SWS_BICUBIC, NULL, NULL, NULL);
 
-    uint32_t y_size = out_width * out_height;
     while (frame_cnt++ < ctx->frames) {
         ifs->read(buf, frame_size);
 
         sws_scale(img_convert_ctx, (const unsigned char * const*)in_data,
                   src_linesize, 0, ctx->height_in,
-                  pFrameYUV->data, pFrameYUV->linesize);
+                  ctx->dst_frame->data, ctx->dst_frame->linesize);
         cout << "frame_cnt " << frame_cnt << endl;
-        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[0]), y_size);
-        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[1]), y_size / 4);
-        ofs.write(reinterpret_cast<char*>(pFrameYUV->data[2]), y_size / 4);
+        write_dst_frame_to_file(ctx);
     }
 
     cout << "End!" << endl;
@@ -103,7 +121,7 @@ int main(int argc, char **argv)
     if (ifs && ifs != &cin)
         delete ifs;
 
-    ofs.close();
+    ctx->ofs->close();
 
     if (buf)
         delete [] buf;
@@ -111,8 +129,8 @@ int main(int argc, char **argv)
     if (out_buffer)
         av_free(out_buffer);
 
-    if (pFrameYUV)
-        av_frame_free(&pFrameYUV);
+    if (ctx->dst_frame)
+        av_frame_free(&ctx->dst_frame);
 
     return 0;
 }
