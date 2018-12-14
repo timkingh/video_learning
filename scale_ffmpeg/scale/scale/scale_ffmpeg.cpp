0a8988a07e46094ca5f73996d783ae18f3602214
timkingh.huang <timkingh.huang@rock-chips.com>
2018-12-14 11:39:39 +0800

    Add RGB format
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- scale_ffmpeg/scale/scale/scale_ffmpeg.cpp
+++ scale_ffmpeg/scale/scale/scale_ffmpeg.cpp
@@ -35,17 +35,23 @@ typedef struct CliCtx {
 static void write_dst_frame_to_file(CliCtx *ctx)
 {
     AVFrame *dst = ctx->dst_frame;
-    uint32_t width = ctx->width_in;
-    uint32_t height = ctx->height_in;
+    ofstream *ofs = ctx->ofs;
+    enum AVPixelFormat dst_fmt = ctx->dst_fmt;
+    uint32_t width = ctx->width_out;
+    uint32_t height = ctx->height_out;
 
-    if (ctx->dst_fmt == AV_PIX_FMT_YUV420P) {
-        uint32_t y_size = ctx->width_in * ctx->height_in;
-        ctx->ofs->write(reinterpret_cast<char*>(dst->data[0]), y_size);
-        ctx->ofs->write(reinterpret_cast<char*>(dst->data[1]), y_size / 4);
-        ctx->ofs->write(reinterpret_cast<char*>(dst->data[2]), y_size / 4);
-    } else if (ctx->dst_fmt == AV_PIX_FMT_RGB24) {
-        ctx->ofs->write(reinterpret_cast<char*>(dst->data[0]),
-                        dst->linesize[0] * height);
+    if (dst_fmt == AV_PIX_FMT_YUV420P) {
+        uint32_t y_size = width * height;
+        ofs->write(reinterpret_cast<char*>(dst->data[0]), y_size);
+        ofs->write(reinterpret_cast<char*>(dst->data[1]), y_size / 4);
+        ofs->write(reinterpret_cast<char*>(dst->data[2]), y_size / 4);
+    } else if (dst_fmt == AV_PIX_FMT_RGB24 || dst_fmt == AV_PIX_FMT_RGBA ||
+               dst_fmt == AV_PIX_FMT_BGRA  || dst_fmt == AV_PIX_FMT_ARGB ||
+               dst_fmt == AV_PIX_FMT_ABGR) {
+        ofs->write(reinterpret_cast<char*>(dst->data[0]),
+                                    dst->linesize[0] * height);
+    } else if (dst_fmt == AV_PIX_FMT_YUYV422) {
+
     }
 }
 
@@ -61,11 +67,13 @@ int main(int argc, char **argv)
     bool help = getarg(false, "-H", "--help", "-?");
     string in_path = getarg("F:\\yuv\\test_cif_420sp.yuv", "-i", "--input");
     string out_path = getarg("F:\\yuv\\test_cif_420p.yuv", "-o", "--output");
-    ctx->width_in = getarg(352, "-w", "--width");
-    ctx->height_in = getarg(288, "-h", "--height");
+    ctx->width_in = getarg(352, "-wi", "--width");
+    ctx->height_in = getarg(288, "-hi", "--height");
+    ctx->width_out = getarg(ctx->width_in, "-wo");
+    ctx->height_out = getarg(ctx->height_in, "-ho");
     ctx->frames = getarg(10, "-f", "--frames");
     ctx->src_fmt = static_cast<enum AVPixelFormat>(getarg(25, "-src_fmt"));
-	ctx->dst_fmt = static_cast<enum AVPixelFormat>(getarg(2, "-dst_fmt"));
+	ctx->dst_fmt = static_cast<enum AVPixelFormat>(getarg(30, "-dst_fmt"));
 
     cout << "input: " << in_path << endl
          << "output: " << out_path << endl
@@ -79,7 +87,6 @@ int main(int argc, char **argv)
     uint32_t frame_size = ctx->width_in * ctx->height_in * 3 / 2;
     char *buf = new char[frame_size];
     uint8_t *in_data[3];
-    uint32_t out_width = ctx->width_in, out_height = ctx->height_in;
 
     /* Todo: impacted by src fmt */
     in_data[0] = (uint8_t *)buf;
@@ -95,15 +102,16 @@ int main(int argc, char **argv)
         cout << "fill src linesize error ret " << ret << endl;
     }
 
-    uint32_t out_frame_size = av_image_get_buffer_size(ctx->dst_fmt, out_width, out_height, 1);
+    uint32_t out_frame_size = av_image_get_buffer_size(ctx->dst_fmt, ctx->width_out, ctx->height_out, 1);
     cout << "buffer size " << out_frame_size << endl;
     out_buffer = (unsigned char *)av_malloc(out_frame_size);
     av_image_fill_arrays(ctx->dst_frame->data, ctx->dst_frame->linesize, out_buffer,
-                         ctx->dst_fmt, out_width, out_height, 1);
+                         ctx->dst_fmt, ctx->width_out, ctx->height_out, 1);
+    cout << "line size " << ctx->dst_frame->linesize[0] << endl;
 
     struct SwsContext *img_convert_ctx;
     img_convert_ctx = sws_getContext(ctx->width_in, ctx->height_in, ctx->src_fmt,
-                                     out_width, out_height, ctx->dst_fmt,
+                                     ctx->width_out, ctx->height_out, ctx->dst_fmt,
                                      SWS_BICUBIC, NULL, NULL, NULL);
 
     while (frame_cnt++ < ctx->frames) {
