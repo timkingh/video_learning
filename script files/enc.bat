0fea4dbf9b62e9599fb783cfe95e6d945dfb974c
timkingh <timkingh@163.com>
2020-02-05 11:42:28 +0800

    Add enc.bat
    Signed-off-by: timkingh <timkingh@163.com>

---
+++ script files/enc.bat
@@ -0,0 +1,16 @@
+
+x264clid.exe ^
+--input-res 1920x1080 ^
+--preset medium ^
+--frames 5 ^
+-I 90 ^
+-q 40 ^
+-o dzq.h264 ^
+--no-scenecut ^
+--rc-lookahead 0 ^
+--aq-mode 0 ^
+--ipratio 1 ^
+--weightp 0 ^
+--bframes 0 ^
+--ref 1 ^
+F:\yuv\dzq_1080p.yuv
