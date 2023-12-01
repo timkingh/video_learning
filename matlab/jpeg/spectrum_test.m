15736e120b3b6c4addf6f7319550b725039ce7b7
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-01 16:02:55 +0800

    Remove blk16 matrix
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/jpeg/spectrum_test.m
+++ matlab/jpeg/spectrum_test.m
@@ -15,28 +15,7 @@ blk8 = [
    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
 ];
 
-% blk16 = [
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-%    min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
-%    max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v max_v   min_v   max_v   min_v   max_v   min_v   max_v   min_v
-% ];
 
 T = dctmtx(8);
-% T16 = dctmtx(16);
 
 coef = T * (blk8 - 128) * T'
-
-% coef16 = T16 * (blk16 - 128) * T16'
