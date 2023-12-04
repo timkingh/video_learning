c05012564cda4c2dc99c55038d7f423634f6694e
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-04 11:41:43 +0800

    Add dct_test.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dct_spectrum/spectrum_test.m
+++ matlab/dct_spectrum/spectrum_test.m
@@ -1,39 +1,88 @@
 clear all
-clc
+%clc
+tic
 
 fid_out = fopen("D:\v2_rkvenc\jpeg\20231201\log.txt", "w");
 T = dctmtx(8);
 min_v = 0;
+max_v = 255;
+value = [ min_v, max_v ];
+iter_num = 100;
+target_coef = 0;
+
+coef_min_max = ones(5, 2);
+coef_min_max(:, 1) = coef_min_max(:, 1) * 2048;
+coef_min_max(:, 2) = coef_min_max(:, 2) * (-2048);
 
 
-for max_v = 0:15:255
-    blk8 = [   min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
-               min_v   min_v   max_v   max_v   min_v   min_v   max_v   max_v
+% order(4): org_min, org_max, coef_min, coef_max
+% pos(5): (1, 1), (3, 4), (4, 1), (6, 3), (8, 8)
+ret_mtx = zeros(8, 8, 20); % 4 * 5
+coord = [ 1, 1;
+          3, 4;
+          4, 1;
+          6, 3;
+          8, 8;
+        ];
+
+
+for m = 1:iter_num
+    blk8 = [ value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
+             value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2)) value(1, randi(2))
             ];
 
     coef = T * (blk8 - 128) * T';
-
-    fprintf(fid_out, "min:max %d:%d\n", min_v, max_v);
-    for m = 1:8
-        for n = 1:8
-            fprintf(fid_out, "%5d ", round(coef(m, n)));
+    coef = round(coef);
+    
+    for n = 1:5
+        target_coef = coef(coord(n, 1), coord(n, 2));
+        
+        if target_coef < coef_min_max(n, 1)
+            coef_min_max(n, 1) = target_coef;
+            ret_mtx(:, :, (n - 1) * 4 + 1) = blk8;
+            ret_mtx(:, :, (n - 1) * 4 + 3) = coef;
         end
-
-        fprintf(fid_out, "\n");
+        
+        if target_coef > coef_min_max(n, 2)
+            coef_min_max(n, 2) = target_coef;
+            ret_mtx(:, :, (n - 1) * 4 + 2) = blk8;
+            ret_mtx(:, :, (n - 1) * 4 + 4) = coef;
+        end
     end
-    fprintf(fid_out, "\n");
 end
 
+str_coord = [ "(1, 1)", "(3, 4)", "(4, 1)", "(6, 3)", "(8, 8)" ];
+log_str = [ "org_min", "org_max", "coef_min", "coef_max" ];
+for j = 1:5
+    
+    fprintf("iter_num %d pos%s coef_min %d coef_max %d\n", ...
+            iter_num, str_coord(1, j), coef_min_max(j, 1), coef_min_max(j, 2));
+    fprintf(fid_out, "iter_num %d pos%s coef_min %d coef_max %d\n", ...
+            iter_num, str_coord(1, j), coef_min_max(j, 1), coef_min_max(j, 2));
+
+    for k = 1:4
+        fprintf(fid_out, "%s\n", log_str(1, k));
+        for m = 1:8
+            for n = 1:8
+                fprintf(fid_out, "%5d ", ret_mtx(m, n, (j - 1) * 4 + k));
+            end
+
+            fprintf(fid_out, "\n");
+        end
+        fprintf(fid_out, "\n");
+    end
+end
 
 fclose(fid_out);
 
 fprintf("finished!\n");
+toc
 
 % 01010101
 %     blk8 = [   min_v   max_v   min_v   max_v   min_v   max_v   min_v   max_v
