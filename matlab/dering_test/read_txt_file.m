782103bf8fc49bab17b85bb4f61159c248fac7d8
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-05 11:51:23 +0800

    Add read_txt_file.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

---
+++ matlab/dering_test/read_txt_file.m
@@ -0,0 +1,34 @@
+clear all
+clc
+
+width = 1280;
+height = 720;
+
+fid_dering0 = fopen('jpg_dec_street_hisi_r7_m3_deringlvl0.txt', 'r');
+fid_dering1 = fopen('jpg_dec_street_hisi_r7_m3_deringlvl1_dct_only.txt', 'r');
+fid_out = fopen('dct_cmp_out.txt', 'w');
+
+blk_num = 0;
+
+for row = 1:16:height
+    for col = 1:16:width
+        for idx = 0:3
+            pos_x = col - 1 + rem(idx, 2) * 8;
+            pos_y = row - 1 + floor(idx / 2) * 8;
+            coef0 = fscanf(fid_dering0, '%d', [8, 8]);
+            coef1 = fscanf(fid_dering1, '%d', [8, 8]);
+            if isequal(coef0, coef1) ~= 1
+                fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
+                        pos_x / 8, pos_y / 8);
+                blk_num = blk_num + 1;
+            end
+        end
+    end
+end
+
+
+fclose(fid_dering0);
+fclose(fid_dering1);
+fclose(fid_out);
+
+fprintf("blk_num %d\n", blk_num);
