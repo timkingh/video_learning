f508db3caddd3c4ce478853b42b786b86fc6392e
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-11 15:28:58 +0800

    Update handle_psnr_bitrate_v2.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/calc_bdrate/calc_bdrate.m
+++ matlab/calc_bdrate/calc_bdrate.m
@@ -6,10 +6,10 @@ clc;
 
 root_path = ".\";
 yuv_path = root_path + "yuv_list.txt";
-psnr_path = root_path + "psnr_all.txt";
-psnr_path2 = root_path + "psnr_rk.txt";
-bitrate_path = root_path + "bitrate_all.txt";
-bitrate_path2 = root_path + "bitrate_rk.txt";
+psnr_path = root_path + "hisi_psnr.txt";
+psnr_path2 = root_path + "rk_psnr.txt";
+bitrate_path = root_path + "hisi_file.txt";
+bitrate_path2 = root_path + "rk_file.txt";
 result_path = root_path + "jpeg_bdrate.txt";
 handle_psnr_bitrate_v2(yuv_path, psnr_path, bitrate_path, ...
                        psnr_path2, bitrate_path2, result_path);
