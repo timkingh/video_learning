3acb5be0e37e314d9f87f0989715f7a425f16073
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-13 10:21:09 +0800

    Update read_txt_file.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/read_txt_file.m
+++ matlab/dering_test/read_txt_file.m
@@ -4,9 +4,21 @@ clc
 width = 1280;
 height = 720;
 
-fid_dering0 = fopen('jpg_dec_street_out27_hisi_r15_dering0.txt', 'r');
-fid_dering1 = fopen('jpg_dec_street_out27_hisi_r15_dering1.txt', 'r');
-fid_out = fopen('dct_cmp_out.txt', 'w');
+fid_dering0 = fopen('jpg_dec_street_out40_hisi_r15_dering0.txt', 'r');
+fid_dering1 = fopen('jpg_dec_street_out40_hisi_r15_dering1.txt', 'r');
+fid_var = fopen('out_var_1.txt', 'r');
+fid_out = fopen('dct_cmp_out_rk_15000.txt', 'w');
+
+qtable = [
+     3   2   2   3   5   8  10  12
+     2   2   3   4   5  12  12  11
+     3   3   3   5   8  11  14  11
+     3   3   4   6  10  17  16  12
+     4   4   7  11  14  22  21  15
+     5   7  11  13  16  21  23  18
+    10  13  16  17  21  24  24  20
+    14  18  19  20  22  20  21  20
+];
 
 blk_num = 0;
 
@@ -17,11 +29,24 @@ for row = 1:16:height
             pos_y = row - 1 + floor(idx / 2) * 8;
             coef0 = fscanf(fid_dering0, '%d', [8, 8]);
             coef1 = fscanf(fid_dering1, '%d', [8, 8]);
-            if isequal(coef0, coef1) ~= 1
-                fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
-                        pos_x / 8, pos_y / 8);
-                blk_num = blk_num + 1;
+            blk8_var = fscanf(fid_var, '%d', 1);
+            for m = 1:8
+                for n = 1:8
+                    if coef0(m, n) ~= coef1(m, n)
+                        rem_v = rem(abs(coef0(m, n) - coef1(m, n)), qtable(m, n));
+                        if rem_v ~= 0
+                            fprintf(fid_out, "pos(%d, %d) blk8_pos(%d, %d)\n", ...
+                                    pos_x, pos_y, m, n);
+                            blk_num = blk_num + 1;   
+                        end
+                    end
+                end
             end
+%             if blk8_var > 15000 % isequal(coef0, coef1) ~= 1
+%                 fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
+%                         pos_x, pos_y);
+%                 blk_num = blk_num + 1;
+%             end
         end
     end
 end
@@ -29,6 +54,7 @@ end
 
 fclose(fid_dering0);
 fclose(fid_dering1);
+fclose(fid_var);
 fclose(fid_out);
 
 fprintf("blk_num %d\n", blk_num);
