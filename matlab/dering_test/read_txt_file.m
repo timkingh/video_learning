6fa72d6893e861915d62fd4b25c98377005dc494
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-14 09:08:41 +0800

    Update read_txt_file.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/read_txt_file.m
+++ matlab/dering_test/read_txt_file.m
@@ -1,5 +1,6 @@
 clear all
 clc
+tic
 
 width = 1280;
 height = 720;
@@ -7,7 +8,7 @@ height = 720;
 fid_dering0 = fopen('jpg_dec_street_out40_hisi_r15_dering0.txt', 'r');
 fid_dering1 = fopen('jpg_dec_street_out40_hisi_r15_dering1.txt', 'r');
 fid_var = fopen('out_var_1.txt', 'r');
-fid_out = fopen('dct_cmp_out_rk_15000.txt', 'w');
+fid_out = fopen('dct_parse_out.txt', 'w');
 
 qtable = [
      3   2   2   3   5   8  10  12
@@ -21,6 +22,7 @@ qtable = [
 ];
 
 blk_num = 0;
+out_mtx = zeros(8, 8);
 
 for row = 1:16:height
     for col = 1:16:width
@@ -30,18 +32,21 @@ for row = 1:16:height
             coef0 = fscanf(fid_dering0, '%d', [8, 8]);
             coef1 = fscanf(fid_dering1, '%d', [8, 8]);
             blk8_var = fscanf(fid_var, '%d', 1);
+            fprintf(fid_out, "pos(%d, %d)\n", pos_x, pos_y);
             for m = 1:8
                 for n = 1:8
-                    if coef0(m, n) ~= coef1(m, n)
-                        rem_v = rem(abs(coef0(m, n) - coef1(m, n)), qtable(m, n));
-                        if rem_v ~= 0
-                            fprintf(fid_out, "pos(%d, %d) blk8_pos(%d, %d)\n", ...
-                                    pos_x, pos_y, m, n);
-                            blk_num = blk_num + 1;   
-                        end
-                    end
+                    out_mtx(m, n) = abs(coef0(m, n) - coef1(m, n)) / qtable(n, m);
                 end
             end
+            
+            for m = 1:8
+                for n = 1:8
+                    fprintf(fid_out, "%d ", out_mtx(n, m));
+                end
+                fprintf(fid_out, "\n");
+            end
+            fprintf(fid_out, "\n");
+
 %             if blk8_var > 15000 % isequal(coef0, coef1) ~= 1
 %                 fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
 %                         pos_x, pos_y);
@@ -58,3 +63,4 @@ fclose(fid_var);
 fclose(fid_out);
 
 fprintf("blk_num %d\n", blk_num);
+toc
