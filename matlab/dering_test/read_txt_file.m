bac5b3864f92e3801f41cf0fa250e5ac4bc8a6c3
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-19 10:46:16 +0800

    Update read_txt_file.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/read_txt_file.m
+++ matlab/dering_test/read_txt_file.m
@@ -5,16 +5,17 @@ tic
 width = 1280;
 height = 720;
 
-fid_dering0 = fopen('jpg_dec_street_720p_hisi_r23_q90_dering0_only.txt', 'r');
-fid_dering1 = fopen('jpg_dec_street_720p_hisi_r23_q90_dering1_only.txt', 'r');
+fid_dering0 = fopen('jpg_dec_random_720p_hisi_r27_qt10_dering0.txt', 'r');
+fid_dering1 = fopen('jpg_dec_random_720p_hisi_r27_qt10_dering1.txt', 'r');
 fid_var = fopen('out_var_street_720p.txt', 'r');
-fid_out = fopen('dec_cmp_out_rk.txt', 'w');
+fid_out = fopen('dec_cmp_out_rk_1.txt', 'w');
 fid_out_0 = fopen('dering0.txt', 'w');
 fid_out_1 = fopen('dering1.txt', 'w');
 
 
 blk_num = 0;
 out_mtx = zeros(8, 8);
+min_idx = 16;
 
 for row = 1:16:height
     for col = 1:16:width
@@ -24,20 +25,31 @@ for row = 1:16:height
             coef0 = fscanf(fid_dering0, '%d', [8, 8]);
             coef1 = fscanf(fid_dering1, '%d', [8, 8]);
             dir = fscanf(fid_var, 'pos(%d, %d) dir %d var %d\n', [4, 1]);
-%             if isequal(coef0, coef1) ~= 1
+            blk_num = 0;
+%             if isequal(coef0, coef1) ~= 1                
+                for m = 1:8
+                    for n = 1:8
+                        if coef0(m, n) == 0
+                            blk_num = blk_num + 1;
+                        end
+                    end
+                end
+                
+                if blk_num < 2
+                fprintf(fid_out, "pos(%d, %d) (%d, %d)\n", pos_x, pos_y, m, n);
+                end
 %                  fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
 %                          pos_x / 8, pos_y / 8);
 %                  blk_num = blk_num + 1;
 %             end
-            if dir(4, 1) > 256
-                 fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
-                         pos_x / 8, pos_y / 8);
-                blk_num = blk_num + 1;
-            end
+%             if dir(4, 1) > 256
+%                  fprintf(fid_out, "frame=0, cu_x=%d, cu_y=%d, cu_size=8, mv_x=0, mv_y=-2\n", ...
+%                          pos_x / 8, pos_y / 8);
+%                 blk_num = blk_num + 1;
+%             end
         end
     end
 end
-
 
 fclose(fid_dering0);
 fclose(fid_dering1);
