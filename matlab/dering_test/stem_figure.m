42c9558d8fabd2d454d8577c9c49f3356c417504
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-21 11:35:13 +0800

    r4:stem_figure.m
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

---
+++ matlab/dering_test/stem_figure.m
@@ -0,0 +1,22 @@
+clear all 
+clc
+
+
+[fid_x1, msg] = fopen("D:\code\video_learning\matlab\dering_test\x1.txt", "r");
+if fid_x1 == -1
+    disp(msg);
+    return;
+end
+
+[fid_y1, msg] = fopen("D:\code\video_learning\matlab\dering_test\y1.txt", "r");
+if fid_y1 == -1
+    disp(msg);
+    return;
+end
+
+x1 = fscanf(fid_x1, '%d');
+y1 = fscanf(fid_y1, '%d');
+
+stem(x1, y1, 'filled');
+
+fclose('all');
