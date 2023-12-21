1a95b5ebaec9d980ce6839d2911f73873707f7e2
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-21 15:37:52 +0800

    r16
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/stem_figure.m
+++ matlab/dering_test/stem_figure.m
@@ -1,8 +1,8 @@
 clear all 
 clc
 
-x1_path = ".\log\x1_r9.txt";
-y1_path = ".\log\y1_r9.txt";
+x1_path = ".\log\x1_r13.txt";
+y1_path = ".\log\y1_r13.txt";
 [fid_x1, msg] = fopen(x1_path, "r");
 if fid_x1 == -1
     disp(msg);
@@ -24,6 +24,30 @@ fclose('all');
 
 hold on
 
+x1_path = ".\log\x1_r14.txt";
+y1_path = ".\log\y1_r14.txt";
+[fid_x1, msg] = fopen(x1_path, "r");
+if fid_x1 == -1
+    disp(msg);
+    return;
+end
+
+[fid_y1, msg] = fopen(y1_path, "r");
+if fid_y1 == -1
+    disp(msg);
+    return;
+end
+
+x1 = fscanf(fid_x1, '%d');
+y1 = fscanf(fid_y1, '%d');
+
+stem(x1, y1, '*b');
+
+fclose('all');
+
+
+hold on
+
 x1_path = ".\log\x1_r10.txt";
 y1_path = ".\log\y1_r10.txt";
 [fid_x1, msg] = fopen(x1_path, "r");
@@ -42,29 +66,5 @@ x1 = fscanf(fid_x1, '%d');
 y1 = fscanf(fid_y1, '%d');
 
 stem(x1, y1, 'or');
-
-fclose('all');
-
-
-hold on
-
-x1_path = ".\log\x1_r7.txt";
-y1_path = ".\log\y1_r7.txt";
-[fid_x1, msg] = fopen(x1_path, "r");
-if fid_x1 == -1
-    disp(msg);
-    return;
-end
-
-[fid_y1, msg] = fopen(y1_path, "r");
-if fid_y1 == -1
-    disp(msg);
-    return;
-end
-
-x1 = fscanf(fid_x1, '%d');
-y1 = fscanf(fid_y1, '%d');
-
-stem(x1, y1, '*b');
 
 fclose('all');
