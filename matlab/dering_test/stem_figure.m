8f73613d393ef81772fe2312621525417f4c8bc1
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-21 19:49:20 +0800

    r40
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/stem_figure.m
+++ matlab/dering_test/stem_figure.m
@@ -1,70 +1,40 @@
 clear all 
 clc
 
-x1_path = ".\log\x1_r13.txt";
-y1_path = ".\log\y1_r13.txt";
-[fid_x1, msg] = fopen(x1_path, "r");
-if fid_x1 == -1
-    disp(msg);
-    return;
+coef = [
+    "75.txt", "76.txt", "77.txt"    
+];
+
+
+file_list = [
+    ".\log_1\x1_r39_A" + coef(1, 1), ".\log_1\y1_r39_A" + coef(1, 1), "sk";
+    ".\log_1\x1_r39_A" + coef(1, 2), ".\log_1\y1_r39_A" + coef(1, 2), "*b"; 
+    ".\log_1\x1_r39_A" + coef(1, 3), ".\log_1\y1_r39_A" + coef(1, 3), "or"; 
+];
+
+[rows, cols] = size(file_list);
+
+for m = 1:rows
+    x1_path = file_list(m, 1);
+    y1_path = file_list(m, 2);
+    [fid_x1, msg] = fopen(x1_path, "r");
+    if fid_x1 == -1
+        disp(msg);
+        return;
+    end
+
+    [fid_y1, msg] = fopen(y1_path, "r");
+    if fid_y1 == -1
+        disp(msg);
+        return;
+    end
+
+    x1 = fscanf(fid_x1, '%d');
+    y1 = fscanf(fid_y1, '%d');
+
+    stem(x1, y1, file_list(m, 3));
+
+    fclose('all');
+
+    hold on
 end
-
-[fid_y1, msg] = fopen(y1_path, "r");
-if fid_y1 == -1
-    disp(msg);
-    return;
-end
-
-x1 = fscanf(fid_x1, '%d');
-y1 = fscanf(fid_y1, '%d');
-
-stem(x1, y1, 'sk');
-
-fclose('all');
-
-hold on
-
-x1_path = ".\log\x1_r14.txt";
-y1_path = ".\log\y1_r14.txt";
-[fid_x1, msg] = fopen(x1_path, "r");
-if fid_x1 == -1
-    disp(msg);
-    return;
-end
-
-[fid_y1, msg] = fopen(y1_path, "r");
-if fid_y1 == -1
-    disp(msg);
-    return;
-end
-
-x1 = fscanf(fid_x1, '%d');
-y1 = fscanf(fid_y1, '%d');
-
-stem(x1, y1, '*b');
-
-fclose('all');
-
-
-hold on
-
-x1_path = ".\log\x1_r10.txt";
-y1_path = ".\log\y1_r10.txt";
-[fid_x1, msg] = fopen(x1_path, "r");
-if fid_x1 == -1
-    disp(msg);
-    return;
-end
-
-[fid_y1, msg] = fopen(y1_path, "r");
-if fid_y1 == -1
-    disp(msg);
-    return;
-end
-
-x1 = fscanf(fid_x1, '%d');
-y1 = fscanf(fid_y1, '%d');
-
-stem(x1, y1, 'or');
-
-fclose('all');
