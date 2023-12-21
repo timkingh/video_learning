02bf398e8615f154a3ec2f13d0ee6c43f7aeb867
timkingh.huang <timkingh.huang@rock-chips.com>
2023-12-21 11:49:51 +0800

    r6
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/read_txt_file.m
+++ matlab/dering_test/read_txt_file.m
@@ -19,8 +19,8 @@ end
 
 fid_var = fopen('.\log\out_var_street_720p.txt', 'r');
 fid_out = fopen('.\log\jpg_dec_coef_test_r1_hisi_r36_qt10_dering_mr2.txt', 'w');
-fid_out_0 = fopen('.\log\x1_r5.txt', 'w');
-fid_out_1 = fopen('.\log\y1_r5.txt', 'w');
+fid_out_0 = fopen('.\log\x1_r6.txt', 'w');
+fid_out_1 = fopen('.\log\y1_r6.txt', 'w');
 
 qtable = ones(8, 8) * 10;
 min_idx = 16;
@@ -43,7 +43,14 @@ for row = 1:16:height
             
             for n = 1:8 % row
                 for m = 1:8 % col
-                    if coef0(m, n) ~= coef1(m, n)
+                    if (n == 8 && m >= 7 && m <= 8) || ...
+                       (n == 7 && m > 7)
+                        calc_flag = 1;
+                    else
+                        calc_flag = 0;
+                    end
+                    
+                    if coef0(m, n) ~= coef1(m, n) && calc_flag == 1
                         out_mtx(m, n) = abs(coef0(m, n) - coef1(m, n)) / qtable(n, m);
                         fprintf(fid_out_0, "%d\n", coef0(m, n));
                         fprintf(fid_out_1, "%d\n", out_mtx(m, n));
