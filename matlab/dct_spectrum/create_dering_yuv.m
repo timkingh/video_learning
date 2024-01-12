a64b421e8880f1c6b09b139bb2049b99202136d3
timkingh.huang <timkingh.huang@rock-chips.com>
2024-01-12 14:29:56 +0800

    r72
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dct_spectrum/create_dering_yuv.m
+++ matlab/dct_spectrum/create_dering_yuv.m
@@ -12,7 +12,7 @@ y_mtx = zeros(height, width);
 uv_mtx = ones(height / 2, width / 2) .* 128;
 
 
-[fid_out, msg] = fopen("D:\code\video_learning\matlab\dct_spectrum\output\coef_test_r69.yuv", "w");
+[fid_out, msg] = fopen("D:\code\video_learning\matlab\dct_spectrum\output\coef_test_r72.yuv", "w");
 if fid_out == -1
     disp(msg);
     return;
@@ -23,10 +23,13 @@ for k = 1:16:height
     for j = 1:16:width
         for idx = 0:3
             pos_x = j - 1 + rem(idx, 2) * 8;
-            pos_y = k - 1 + floor(idx / 2) * 8;       
-            if blk8_cnt < 4
-                y_mtx(pos_y + 1:pos_y + 8, pos_x + 1:pos_x + 8) = min_blk8(:, :, blk8_cnt);
-                blk8_cnt = blk8_cnt + 1;
+            pos_y = k - 1 + floor(idx / 2) * 8;   
+            rand_v = randi([0, 255], 1, 15);
+            sort_v = sort(rand_v);
+            for row = 0:7
+                for col = 0:7
+                    y_mtx(pos_y + 1 + row, pos_x + 1 + col) = sort_v(1, row + col + 1);
+                end
             end
         end
     end
