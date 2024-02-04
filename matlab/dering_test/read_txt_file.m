f34840e8434235dfd74e747351e5b43fdab243cf
timkingh.huang <timkingh.huang@rock-chips.com>
2024-02-04 15:37:00 +0800

    r85
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dering_test/read_txt_file.m
+++ matlab/dering_test/read_txt_file.m
@@ -4,16 +4,16 @@ tic
 
 width = 1920;
 height = 1080;
-% qtable = [
-%   3   2   2   3   5   8  10  12
-%   2   2   3   4   5  12  12  11
-%   3   3   3   5   8  11  14  11
-%   3   3   4   6  10  17  16  12
-%   4   4   7  11  14  22  21  15
-%   5   7  11  13  16  21  23  18
-%  10  13  16  17  21  24  24  20
-%  14  18  19  20  22  20  21  20
-% ];
+qtable = [
+     14  10   9  14  22  36  46  55 
+     11  11  13  17  23  52  54  50 
+     13  12  14  22  36  51  62  50 
+     13  15  20  26  46  78  72  56 
+     16  20  33  50  61  98  93  69 
+     22  32  50  58  73  94 102  83 
+     44  58  70  78  93 109 108  91 
+     65  83  86  88 101  90  93  89 
+];
 
 max_bias = zeros(8, 8, 20);
 coef_cnt = zeros(8, 8);
@@ -22,49 +22,36 @@ file_name = ".txt";
 path_name = ".\input_file\jpg_dec_street_1080p_part_hisi_r73_pos_384_152";
 xy_path = path_name + file_name;
 fid_out_xy = fopen(xy_path, 'w');
+ret_mtx = zeros(256, 1);
 
-for quant_value = 1:1:99
+for quant_value = 1:1:1
     qt_val = quant_value;
     qtable = ones(8, 8) .* qt_val;
     fprintf("qf %d\n", qt_val);
 
-    file_path = ".\input_file\jpg_dec_street_1080p_part_hisi_r73_qf" + num2str(qt_val) + "_dering0.txt";
+    file_path = ".\dr0\ave.txt";
     [fid_dering0, msg] = fopen(file_path, 'r');
     if fid_dering0 == -1
         disp(msg);
         return;
     end
 
-    file_path = ".\input_file\jpg_dec_street_1080p_part_hisi_r73_qf" + num2str(qt_val) + "_dering1.txt";
+    file_path = ".\dr1\ave.txt";
     [fid_dering1, msg] = fopen(file_path, 'r');
     if fid_dering1 == -1
         disp(msg);
         return;
     end
 
-    for row = 1:16:height
-        for col = 1:16:width
-            for idx = 0:3
+    for row = 1:8:height
+        for col = 1:8:width
+            for idx = 0:0
                 pos_x = col - 1 + rem(idx, 2) * 8;
                 pos_y = row - 1 + floor(idx / 2) * 8;
-                fscanf(fid_dering0, 'pos(%d, %d)\nplane 0 dct coef:\n');
-                fscanf(fid_dering1, 'pos(%d, %d)\nplane 0 dct coef:\n');
-                coef0 = fscanf(fid_dering0, '%d', [8, 8]);
-                coef1 = fscanf(fid_dering1, '%d', [8, 8]);
-                fscanf(fid_dering0, '\n');
-                fscanf(fid_dering1, '\n');                 
-
-                if pos_x == 384 && pos_y == 152
-                   for n = 1:8 % row
-                        for m = 1:8 % col
-                            if coef0(m, n) ~= coef1(m, n)
-                                coef_cnt(m, n) = coef_cnt(m, n) + 1;
-                                fprintf(fid_out_xy, "qf %d coef(%d, %d) %d %d\n", ...
-                                        quant_value, n, m, coef0(m, n), coef1(m, n));
-                            end  
-                        end
-                    end
-                end               
+                ret0 = fscanf(fid_dering0, 'pos(%d, %d) %d\n', 3);
+                ret1 = fscanf(fid_dering1, 'pos(%d, %d) %d\n', 3);         
+                ret_diff = abs(ret0(3, 1) - ret1(3, 1));
+                ret_mtx(ret_diff + 1, 1) = ret_mtx(ret_diff + 1, 1) + 1;
             end
         end
     end
@@ -73,7 +60,7 @@ for quant_value = 1:1:99
     fclose(fid_dering1);
 end
 
-coef_cnt'
+stem(1:256, ret_mtx');
 
 fprintf("finished\n");
 fclose(fid_out_xy);
