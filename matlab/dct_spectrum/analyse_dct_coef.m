d25bdcfbf5a1f2325bc09d08bcbf22e73da21c84
timkingh.huang <timkingh.huang@rock-chips.com>
2024-02-06 15:47:20 +0800

    r93
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/dct_spectrum/analyse_dct_coef.m
+++ matlab/dct_spectrum/analyse_dct_coef.m
@@ -3,36 +3,49 @@ clc
 
 width = 1920;
 height = 1080;
-
-file_path = "D:\code\video_learning\matlab\dering_test\input_file\";
-file_name = file_path + "jpg_dec_street_1080p_part_hisi_r73_qf56_dering0.txt";
-[coef0, pos0] = open_jpeg_coef_file(file_name, width, height);
-
-file_name = file_path + "jpg_dec_street_1080p_part_hisi_r73_qf56_dering1.txt";
-[coef1, pos1] = open_jpeg_coef_file(file_name, width, height);
-
 blk8_num = width / 8 * height / 8;
 blk8_dr0 = zeros(8, 8);
 blk8_dr1 = zeros(8, 8);
-
 qtable_delta = [
      0 0 0 0 0 0 0 0
      0 0 0 0 0 0 0 0
      0 0 0 0 0 0 0 0
-     0 1 1 1 0 0 0 0
-     0 0 1 1 0 0 0 0
+     0 1 0 1 0 0 0 0
      0 0 1 0 0 0 0 0
+     1 0 1 0 0 0 0 0
      0 0 0 0 0 0 0 0
      0 0 0 0 0 0 0 0
 ];
 
-for iter = 1:blk8_num
-    blk8_dr0 = coef0(:, :, iter);
-    blk8_dr1 = coef1(:, :, iter);
-    pos_x = pos0(iter, 1);
-    pos_y = pos0(iter, 2);
+for qf = 50:59
+    file_path = "D:\\code\\video_learning\\matlab\\dering_test\\input_file\\";
+    file_path0 = file_path + "jpg_dec_street_1080p_part_hisi_r73_qf%d_dering0.txt";
+    file_name = sprintf(file_path0, qf);
+    [coef0, pos0] = open_jpeg_coef_file(file_name, width, height);
+
+    file_path1 = file_path + "jpg_dec_street_1080p_part_hisi_r73_qf%d_dering1.txt";
+    file_name = sprintf(file_path1, qf);
+    [coef1, pos1] = open_jpeg_coef_file(file_name, width, height);
+    fprintf("qf %d\n", qf);
     
-    if isequal(qtable_delta, abs(blk8_dr0 - blk8_dr1))
-        fprintf("pos(%d, %d)\n", pos_x, pos_y);
+    for iter = 1:blk8_num
+        blk8_dr0 = coef0(:, :, iter);
+        blk8_dr1 = coef1(:, :, iter);
+        pos_x = pos0(iter, 1);
+        pos_y = pos0(iter, 2);
+
+        coef_delta = zeros(8, 8);
+        count = 0;
+        for r = 1:8
+            for c = 1:8
+                if blk8_dr0(r, c) ~= blk8_dr1(r, c) && (qtable_delta(r, c) == 1)
+                    count = count + 1;
+                end
+            end
+        end
+
+        if count >= 5
+            fprintf("qf %d pos(%d, %d) count %d\n", qf, pos_x, pos_y, count);
+        end
     end
 end
