61f3f77ef89b264c35283550b2805f810f80ca26
timkingh.huang <timkingh.huang@rock-chips.com>
2024-07-29 10:49:38 +0800

    r98
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/calc_bdrate/calc_bdrate.m
+++ matlab/calc_bdrate/calc_bdrate.m
@@ -6,10 +6,10 @@ clc;
 
 root_path = ".\input_file\";
 yuv_path = root_path + "yuv_list_subj.txt";
-psnr_path = root_path + "hevc_psnr_rk3576_test23.txt";
-psnr_path2 = root_path + "hevc_psnr_rk3576.txt";
-bitrate_path = root_path + "hevc_filesize_rk3576_test23.txt";
-bitrate_path2 = root_path + "hevc_filesize_rk3576.txt";
+psnr_path = root_path + "avc_psnr_rv1106_test1.txt";
+psnr_path2 = root_path + "avc_psnr_rv1106.txt";
+bitrate_path = root_path + "avc_filesize_rv1106_test1.txt";
+bitrate_path2 = root_path + "avc_filesize_rv1106.txt";
 result_path = root_path + "bdrate.txt";
 handle_psnr_bitrate_v2(yuv_path, psnr_path, bitrate_path, ...
                        psnr_path2, bitrate_path2, result_path);
