05b59316a81172c6e5b12df53d2c9ad42e1c6cac
timkingh.huang <timkingh.huang@rock-chips.com>
2024-08-21 11:15:52 +0800

    h264 calc
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/calc_bdrate/calc_bdrate.m
+++ matlab/calc_bdrate/calc_bdrate.m
@@ -6,10 +6,10 @@ clc;
 
 root_path = ".\input_file\";
 yuv_path = root_path + "yuv_list_subj.txt";
-psnr_path = root_path + "hevc_psnr_rv1103b_test4.txt";
-psnr_path2 = root_path + "hevc_psnr_rv1103b.txt";
-bitrate_path = root_path + "hevc_filesize_rv1103b_test4.txt";
-bitrate_path2 = root_path + "hevc_filesize_rv1103b.txt";
+psnr_path = root_path + "avc_psnr_rk3576_test1.txt";
+psnr_path2 = root_path + "avc_psnr_rk3576.txt";
+bitrate_path = root_path + "avc_filesize_rk3576_test1.txt";
+bitrate_path2 = root_path + "avc_filesize_rk3576.txt";
 result_path = root_path + "bdrate.txt";
 handle_psnr_bitrate_v2(yuv_path, psnr_path, bitrate_path, ...
                        psnr_path2, bitrate_path2, result_path);
