9c7cf95d31c8bf70238e470df3f1e3e644a6be9d
timkingh.huang <timkingh.huang@rock-chips.com>
2025-04-27 15:55:22 +0800

    r127:matlab: Modify filter demo
    Signed-off-by: timkingh.huang <timkingh.huang@rock-chips.com>

--- matlab/filter_md/filter_demo.m
+++ matlab/filter_md/filter_demo.m
@@ -1,9 +1,30 @@
+clc
+clear
+
 
 file = 'D:\code\video_learning\matlab\filter_md\test_854x480_yuv400.yuv';
+file_filt = 'D:\code\video_learning\matlab\filter_md\test_854x480_yuv400_filt.yuv';
+file_md = 'D:\code\video_learning\matlab\filter_md\test_854x480_yuv400_md.yuv';
+
 fid = fopen(file, 'rb');
+if fid == -1
+    error('fopen %s file', file);
+end
+
+fid_filt = fopen(file_filt, 'w+');
+if fid_filt == -1
+    error('fopen %s file', file_filt);
+end
+
+fid_md = fopen(file_md, 'w+');
+if fid_md == -1
+    error('fopen %s file', file_md);
+end
+
+
 width = 854;
 height = 480;
-frame_num = 1;
+frame_num = 495;
 
 filter_radius = 25;
 filter_sigma = filter_radius;
@@ -15,10 +36,16 @@ for i = 1:frame_num
     y_filter = imfilter(y, filter_kernel, 'replicate');
     
     y_detail = abs(y - y_filter);
+    y_detail_int = uint8(y_detail);
     
-    figure, imshow(y/255)
-    figure, imshow(y_filter/255)
-    figure, imshow(y_detail/255)
+    %figure, imshow(y/255)
+    %figure, imshow(y_filter/255)
+    %figure, imshow(y_detail/255)
+    
+    fwrite(fid_filt, uint8(y_filter'), 'uint8');
+    fwrite(fid_md, uint8(y_detail_int'), 'uint8');
 end
 
 fclose(fid);
+fclose(fid_filt);
+fclose(fid_md);
